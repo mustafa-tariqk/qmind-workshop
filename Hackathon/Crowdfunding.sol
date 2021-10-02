5367a0d5c6d0399e150a504b4b456a1d0e43a173
mustafa-tariqk <mustafa-tariqk@pm.me>
2021-10-02 18:12:34 -0400

    refactoring and creation of hackathon

---
+++ Hackathon/Crowdfunding.sol
@@ -0,0 +1,162 @@
+// We will be using Solidity version 0.5.4
+pragma solidity 0.5.4;
+
+
+contract Crowdfunding {
+    using SafeMath for uint256;
+
+    // List of existing projects
+    Project[] private projects;
+
+    // Event that will be emitted whenever a new project is started
+    event ProjectStarted(
+        address contractAddress,
+        address projectStarter,
+        string projectTitle,
+        string projectDesc,
+        uint256 deadline,
+        uint256 goalAmount
+    );
+
+    /** @dev Function to start a new project.
+      * @param title Title of the project to be created
+      * @param description Brief description about the project
+      * @param durationInDays Project deadline in days
+      * @param amountToRaise Project goal in wei
+      */
+    function startProject(
+        string calldata title,
+        string calldata description,
+        uint durationInDays,
+        uint amountToRaise
+    ) external {
+        uint raiseUntil = now.add(durationInDays.mul(1 days));
+        Project newProject = new Project(msg.sender, title, description, raiseUntil, amountToRaise);
+        projects.push(newProject);
+        emit ProjectStarted(
+            address(newProject),
+            msg.sender,
+            title,
+            description,
+            raiseUntil,
+            amountToRaise
+        );
+    }                                                                                                                                   
+
+    /** @dev Function to get all projects' contract addresses.
+      * @return A list of all projects' contract addreses
+      */
+    function returnAllProjects() external view returns(Project[] memory){
+        return projects;
+    }
+}
+
+
+contract Project {
+    using SafeMath for uint256;
+    
+    // Data structures
+    enum State {
+        Fundraising,
+        Expired,
+        Successful
+    }
+
+    // State variables
+    address payable public creator;
+    uint public amountGoal; // required to reach at least this much, else everyone gets refund
+    uint public completeAt;
+    uint256 public currentBalance;
+    uint public raiseBy;
+    string public title;
+    string public description;
+    State public state = State.Fundraising; // initialize on create
+    mapping (address => uint) public contributions;
+
+    // Event that will be emitted whenever funding will be received
+    event FundingReceived(address contributor, uint amount, uint currentTotal);
+    // Event that will be emitted whenever the project starter has received the funds
+    event CreatorPaid(address recipient);
+
+    // Modifier to check current state
+    modifier inState(State _state) {
+        require(state == _state);
+        _;
+    }
+
+    // Modifier to check if the function caller is the project creator
+    modifier isCreator() {
+        require(msg.sender == creator);
+        _;
+    }
+
+    constructor
+    (
+        address payable projectStarter,
+        string memory projectTitle,
+        string memory projectDesc,
+        uint fundRaisingDeadline,
+        uint goalAmount
+    ) public {
+        creator = projectStarter;
+        title = projectTitle;
+        description = projectDesc;
+        amountGoal = goalAmount;
+        raiseBy = fundRaisingDeadline;
+        currentBalance = 0;
+    }
+
+    /** @dev Function to fund a certain project.
+      */
+    function contribute() external inState(State.Fundraising) payable {
+        
+        // CODE HERE
+
+    }
+
+    /** @dev Function to change the project state depending on conditions.
+      */
+    function checkIfFundingCompleteOrExpired() public {
+        
+        // CODE HERE
+
+    }
+
+    /** @dev Function to give the received funds to project starter.
+      */
+    function payOut() internal inState(State.Successful) returns (bool) {
+
+        // CODE HERE
+
+    }
+
+    /** @dev Function to retrieve donated amount when a project expires.
+      */
+    function getRefund() public inState(State.Expired) returns (bool) {
+        
+        // CODE HERE
+
+    }
+
+    /** @dev Function to get specific information about the project.
+      * @return Returns all the project's details
+      */
+    function getDetails() public view returns 
+    (
+        address payable projectStarter,
+        string memory projectTitle,
+        string memory projectDesc,
+        uint256 deadline,
+        State currentState,
+        uint256 currentAmount,
+        uint256 goalAmount
+    ) {
+        projectStarter = creator;
+        projectTitle = title;
+        projectDesc = description;
+        deadline = raiseBy;
+        currentState = state;
+        currentAmount = currentBalance;
+        goalAmount = amountGoal;
+    }
+}
